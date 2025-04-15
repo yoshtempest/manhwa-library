726c37b50cbb66e4ca8b8252dd1772d8c0dc19ff
Marcelo <marcelobenjaminrocha100@gmail.com>
2025-04-15 20:38:36 -0300

    initializating project

---
+++ src/components/button.tsx
@@ -0,0 +1,7 @@
+export default function MeuBotao() {
+    return (
+      <button className="bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-800">  Eu sou um botão mágico!
+    {/* //   <button style={{ backgroundColor: 'purple', color: 'white' }}> */}
+      </button>
+    )
+}
