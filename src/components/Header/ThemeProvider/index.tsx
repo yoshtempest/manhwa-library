41605d1a58b6d6218437b10dd2e13364a555c679
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-04-28 11:41:25 -0300

    feat: background color of dark theme is close to finished

--- src/components/Header/ThemeProvider/index.tsx
+++ src/components/Header/ThemeProvider/index.tsx
@@ -7,27 +7,40 @@ import DarkTheme from '../../../../assets/DarkTheme.svg'
 
 import { useEffect, useState } from "react";
 
-export function ThemeProvider() {
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [theme, setTheme] = useState<"light" | "dark">("light");
 
+  // 1ª Execução: Verifica preferência do usuário
   useEffect(() => {
-    // Respeita a preferência do sistema apenas na primeira carga
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-    }
+    const storedTheme = localStorage.getItem("theme") as "light" | "dark";
+    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    
+    setTheme(storedTheme || (systemPrefersDark ? "dark" : "light"));
   }, []);
 
+  // Sempre que o tema mudar: atualiza HTML e localStorage
   useEffect(() => {
+    // Aplica a classe no elemento <html>
     document.documentElement.className = theme;
+    // Salva preferência
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
-    <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      {theme === "light" ? (
-          <Icon iconPath={LightTheme.src} alt="Icon of light theme" />
-      ) : (
-          <Icon iconPath={DarkTheme.src} alt="Icon of dark theme" />
-      )}
-    </button>
+    <>
+      {/* Botão Toggle */}
+      <button
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        aria-label={`${theme === "light" ? "escuro" : "claro"}`}
+      >
+        <Icon 
+          iconPath={theme === "light" ? DarkTheme.src : LightTheme.src} 
+          alt={`Ícone de tema ${theme === "light" ? "escuro" : "claro"}`} 
+        />
+      </button>
+      
+      {/* Conteúdo da aplicação */}
+      {children}
+    </>
   );
 }
