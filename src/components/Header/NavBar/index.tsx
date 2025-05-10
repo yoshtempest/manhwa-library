acc1163fc9ea4b5f0fcaff6fed5e3df278d3a56b
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-05-10 15:10:45 -0300

    fix: borders in NavBar are removed and icon has placed to input container

--- src/components/Header/NavBar/index.tsx
+++ src/components/Header/NavBar/index.tsx
@@ -3,24 +3,23 @@ import Icon from "@/components/Icon";
 import SearchIcon from '@/assets/SearchIcon.svg'
 import SearchIconWhite from '@/assets/SearchIconWhite.svg'
 
-
 const NavBar = () => {
     return (
         <div className={styles.Container}>
-            <div className={styles.InputContainer}>
-                <input className={styles.Input}
-                placeholder="Digite aqui">
-                </input>
-            </div>
-
-            <div className={styles.IconContainer}>
-                <Icon 
-                iconPath={SearchIconWhite.src}
-                alt="Search icon"/>
+            <div className={styles.InputWrapper}>
+                <input 
+                    className={styles.Input}
+                    placeholder="Digite aqui"
+                />
+                <div className={styles.SearchIcon}>
+                    <Icon 
+                        iconPath={SearchIconWhite.src}
+                        alt="Search icon"
+                    />
+                </div>
             </div>
         </div>
     )
 }
-
 
 export default NavBar;
