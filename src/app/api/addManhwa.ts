f195b1e432ef304a5c95fd3006638b4033b6fbfb
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-05-19 12:38:15 -0300

    new: comenting the code

--- src/app/api/addManhwa.ts
+++ src/app/api/addManhwa.ts
@@ -4,25 +4,21 @@ import sqlite3 from 'sqlite3';
 
 // Função para garantir que a tabela existe
 async function ensureTable() {
-  // abre a conexão
+  // Abre a conexão
   const db = await open({
     filename: './src/manhwas.db',
     driver: sqlite3.Database
   });
 
-  // espera o banco de dados estar pronto
+  // Espera o banco de dados estar pronto
   await db.run(
     "INSERT INTO manhwas (title, genres) VALUES (?, ?)",
-    ["Teste", "Ação"]
+    ["Teste", "Ação"] // TESTE QUE NÃO FUNCIONOU, SUPONHO TER QUE EXPORTAR A FUNÇÃO DE ABRIR/CRIAR O BANCO DE DADOS PARA QUE EU A IMPORTE AQUI E PODER INSERIR MEUS MANHWAS APÓS ISSO.
   );
 
   const result = await db.get("SELECT * FROM manhwas");
   console.log("Primeiro manhwa inserido:", result);
 }
-// Rode manualmente apenas em ambiente de desenvolvimento para criar a tabela
+// ensureTable(); // Para rodar o código basta descomentar esta linha e usar npm run dev.
 
-// ensureTable(); // para rodar o código basta descomentar esta linha
-
-// rodar o servidor novamente
-// e depois comentar novamente
-// para evitar que a tabela seja criada toda vez que o servidor reiniciar
+// Depois comente novamente para evitar que a tabela seja criada toda vez que o servidor reiniciar.
