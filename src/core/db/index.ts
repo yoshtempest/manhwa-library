bf9ad3409ff2e0a0ed17344aa3f078fe8a2d75fa
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-05-23 15:05:51 -0300

    new: Commenting about the code and drawing the api

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -1,12 +1,20 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
+// Define o tipo SqLiteDatabase como o tipo retornado pela função open()
 type SqliteDatabase = Awaited<ReturnType<typeof open>>;
 
+
+/* 
+    Classe Responsável por gerenciar a conexão com o banco de dados Sqlite
+    Garante que apenas uma conexão seja aberta por vez(singleton)
+    Fornece métodos para conectar, obter a sessão, criar e remover tabelas, e desconectar.
+*/
 class DBConnectionHandler {
     private db: SqliteDatabase | null = null;
-    private readonly dbPath = 'library.db';
+    private readonly dbPath = 'library.db'; // Caminho do arquivo do banco de dados.
 
+    // Conecta ao banco de dados, caso não esteja conectado
     async connect() {
         if (!this.db) {
             this.db = await open({
@@ -15,14 +23,20 @@ class DBConnectionHandler {
             });
         }
     }
-
+    /*
+        Retorna a sessão ativa ao banco de dados.
+        Se não houver uma sessão ativa, chama o método connect() para criar uma nova.
+    */
     async getSession(): Promise<SqliteDatabase> {
         if (!this.db) {
             await this.connect()
         }
         return this.db!;
     }
-
+    /*
+        Cria as tabelas necessárias no banco de dados.
+        Caso a tabela já exista, não faz nada.
+    */
     async createTables() {
         if (!this.db) throw new Error('Database not connected');
         await this.db.exec(`
@@ -37,12 +51,12 @@ class DBConnectionHandler {
             );
         `);
     }
-
+    // Remove as tabelas do banco de dados.
     async dropTables() {
         if (!this.db) throw new Error('Database not connected');
         await this.db.exec(`DROP TABLE IF EXISTS users`);
     }
-
+    // Encerra a conexão com o banco de dados.
     async disconnect() {
         if (this.db) {
             await this.db.close();
@@ -52,6 +66,7 @@ class DBConnectionHandler {
 }
 
 
+// Instancia a classe DBConnectionHandler e conecta ao banco de dados.
 const db = new DBConnectionHandler();
 db.connect()
 
