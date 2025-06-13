a7aefdcae4db73540289bae57a1a1fc57cafb387
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-06-13 12:20:27 -0300

    new: registerPage

--- src/app/api/register/page.tsx
+++ src/app/api/register/page.tsx
@@ -1,23 +1,25 @@
 import styles from "./styles.module.css";
 
 
-// Todos os inputs serão iguais(só muda quando muda o tema) então dá pra componentizar
-const Login = () => {
+const Register = () => {
     return (
         <div className={styles.Container}>
             <h1>Login</h1>
             <form>
-                <label htmlFor="email">e-mail:</label>
+                <label htmlFor="email">E-mail:</label>
                 <input type="text" id="email" name="email" placeholder="seu@email.com" required />
+
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" required />
                 
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password" required />
                 
-                <button type="submit">Entrar</button>
+                <button type="submit">Concluir</button>
             </form>
         </div>
     );
 }
 
 
-export default Login;
+export default Register;
