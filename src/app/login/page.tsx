90f483b79f1a2e550416b2dca57fd57690cf464d
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-06-17 10:36:37 -0300

    fix: removed the <a> to conclude and entry from register and login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,7 +17,8 @@ const Login = () => {
                         <a href="/register" className={styles.Link}> Cadastrar-se</a>
                     </p>
 
-                <button type="submit" className={styles.Submit}><a href="/">Entrar</a></button>
+                <button type="submit" className={styles.Submit}>Entrar</button>
+
             </form>
         </div>
     );
