34a930279635cd00f70fac30900a81d3ba4f45c1
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-06-18 11:21:40 -0300

    new: register validation

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,26 +1,59 @@
+"use client";
+
 import styles from "./styles.module.css";
 import Input from "@/components/Input";
+import { useRouter } from "next/navigation"
+import { useState } from "react";
 
 
 const Register = () => {
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (email && username && password) {
+            return router.push('/login')
+        }
+        else {
+            alert('Preencha todos os campos!');
+        }
+    } 
     return (
         <div className={styles.Container}>
             <h1 className={styles.Title}>Register</h1>
             <form className={styles.Form}>
 
                 <label htmlFor="email">E-mail:</label>
-                <Input placeholder="seu@email.com" type="email"/>
+                <Input
+                    placeholder="seu@email.com"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
 
                 <label htmlFor="username">Username:</label>
-                <Input placeholder="Robisvaldo Ratimbum"/>
+                <Input
+                    placeholder="Robisvaldo Ratimbum"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
                 
                 <label htmlFor="password">Password:</label>
-                <Input placeholder="senhaSegura123" type="password"/>
+                <Input
+                    placeholder="senhaSegura123"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                
                 <p className={styles.Text}> Já possui uma conta?
                     <a href="/login" className={styles.Link}> Ir para o login</a>
                 </p>
   
-                <button type="submit" className={styles.Submit}>Concluir</button>
+                <button type="submit" className={styles.Submit} onClick={handleSubmit}>Concluir</button>
             </form>
         </div>
     );
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,16 +4,22 @@ import styles from './styles.module.css';
 interface Props {
     placeholder: string;
     type?: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input = ({
     placeholder,
     type,
+    value,
+    onChange,
 } : Props) => {
     return (
         <input className={styles.Input}
             placeholder={placeholder}
             type={type}
+            value={value}
+            onChange={onChange}
             required
         />
     );
