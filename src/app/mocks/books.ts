579b8fc1d31a4de1e31ec4e29cdfadb946784003
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-06-24 07:56:10 -0300

    new: any manhwas

--- src/app/mocks/books.ts
+++ src/app/mocks/books.ts
@@ -1,33 +1,35 @@
 import { BookResponse } from '@/schemas/book';
+import IMAGES from '@/uploads';
+
 
 export async function getBooks():  Promise<BookResponse[]> {
     return [
         {
             id: "1",
             title: "Magic Emperor",
-            author: "Zhuo Yifan",
-            genres: "Fantasy, Adventure",
-            description: "Follow the journey of",
+            author: "Ye Xiao",
+            genres: "Action, Adventure, Shounen",
+            description: "Zhuo Yifan era um imperador mágico ou poderia ser chamado de imperador demônio porque ele tinha um livro do imperador antigo chamado Livro dos Nove Segredos, ele foi alvo de todos os especialistas e foi traído e morto por seus alunos. Então sua alma entra e volta à vida em um servo da família chamado Zhuo Fan. Alguma magia demoníaca o impede, ele deve unir as memórias de criança e não pode ignorar a família e a amante a quem serve.",
             note: 4.5,
-            image_path: "https://example.com/magic-emperor.jpg"
+            image_path: IMAGES.magicEmperor
         },
         {
             id: "2",
-            title: "Magic Emperor",
-            author: "Zhuo Yifan",
-            genres: "Fantasy, Adventure",
-            description: "Follow the journey of",
-            note: 4.5,
-            image_path: "https://example.com/magic-emperor.jpg"
+            title: "Solo Leveling",
+            author: "Chugong",
+            genres: "Action, Fantasy",
+            description: "Em um mundo onde caçadores humanos que possuem habilidades mágicas devem lutar contra monstros mortais para proteger a raça humana de certa aniquilação, um caçador notoriamente fraco chamado Sung Jinwoo se encontra em uma luta aparentemente interminável pela sobrevivência. Um dia, depois de sobreviver por pouco a uma masmorra dupla esmagadoramente poderosa que quase acaba com todo o seu grupo, um programa misterioso chamado Sistema o escolhe como seu único jogador e, por sua vez, dá a ele a habilidade extremamente rara de subir de nível em força, possivelmente além de qualquer limites conhecidos. Jinwoo então parte em uma jornada enquanto luta contra todos os tipos de inimigos, tanto homens quanto monstros, para descobrir os segredos das masmorras e a verdadeira fonte de seus poderes.",
+            note: 5,
+            image_path: IMAGES.soloLeveling
         },
         {
             id: "3",
-            title: "Magic Emperor",
-            author: "Zhuo Yifan",
-            genres: "Fantasy, Adventure",
-            description: "Follow the journey of",
+            title: "Max-Level Newbie",
+            author: "Maslow",
+            genres: "Action, Fantasy",
+            description: "Jinhyuk, um viciado completo, foi o único a ter visto o final da [Torre dos Testes], mas à medida que a popularidade do jogo diminui, fica difícil manter a vida com o jogo. Jinhyuk quer terminar o jogo assim porque ele viu o final. Naquele mesmo dia, a [Torre dos Testes] se tornou uma realidade. Jinhyuk, que conhece todos os elementos do jogo, controla tudo mais rápido do que qualquer um! “Vou te mostrar o que é um profissional de verdade.”",
             note: 4.5,
-            image_path: "https://example.com/magic-emperor.jpg"
+            image_path: IMAGES.novatoDeNivelMaximo
         }
     ]
 }
--- src/uploads/index.ts
+++ src/uploads/index.ts
@@ -1,8 +1,12 @@
 import soloLeveling from './solo_leveling.svg';
+import magicEmperor from './magic_emperor.svg';
+import novatoDeNivelMaximo from './novato_de_nivel_maximo.svg';
 
 
 const IMAGES = {
     soloLeveling,
+    magicEmperor,
+    novatoDeNivelMaximo,
 }
 
 
