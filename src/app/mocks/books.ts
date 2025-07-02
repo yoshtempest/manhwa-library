ebc6021f9132c9e2b73bffd0cbbf29a1b63d47ad
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-07-02 16:17:19 -0300

    fix: size and note of solo leveling image

--- src/app/mocks/books.ts
+++ src/app/mocks/books.ts
@@ -20,7 +20,7 @@ export async function getBooks(bookTitle?: string):  Promise<BookResponse[]> {
             author: "Chugong",
             genres: "Action, Fantasy",
             description: "Em um mundo onde caçadores humanos que possuem habilidades mágicas devem lutar contra monstros mortais para proteger a raça humana de certa aniquilação, um caçador notoriamente fraco chamado Sung Jinwoo se encontra em uma luta aparentemente interminável pela sobrevivência. Um dia, depois de sobreviver por pouco a uma masmorra dupla esmagadoramente poderosa que quase acaba com todo o seu grupo, um programa misterioso chamado Sistema o escolhe como seu único jogador e, por sua vez, dá a ele a habilidade extremamente rara de subir de nível em força, possivelmente além de qualquer limites conhecidos. Jinwoo então parte em uma jornada enquanto luta contra todos os tipos de inimigos, tanto homens quanto monstros, para descobrir os segredos das masmorras e a verdadeira fonte de seus poderes.",
-            note: 5,
+            note: 4.5,
             image_path: IMAGES.soloLeveling.src
         },
         {
