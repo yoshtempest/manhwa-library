c50806e9e1ae7987161e3b525f5785ab2e50982b
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-07-10 13:20:58 -0300

    fix: changing some of // to /** + */

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,31 +3,31 @@ import UserService from "@/services/user";
 import { UserLogin } from "@/schemas/user";
 import SecurityHandler from "@/core/security";
 
-/*
+/**
   Função responsável por autenticar um usuário.
   
-  1. Recebe os dados de login via request.
-  2. Cria uma sessão de banco de dados.
-  3. Instancia o serviço de usuário.
-  4. Tenta autenticar o usuário.
-  5. Retorna erro 401 se as credenciais forem inválidas.
-  6. Retorna os dados do usuário autenticado em caso de sucesso.
- */
+  - Recebe os dados de login via request.
+  - Cria uma sessão de banco de dados.
+  - Instancia o serviço de usuário.
+  - Tenta autenticar o usuário.
+  - Retorna erro 401 se as credenciais forem inválidas.
+  - Retorna os dados do usuário autenticado em caso de sucesso.
+*/
 export async function POST(request: Request): Promise<Response> {
     try {
-        // 1. Recebe e valida os dados de login enviados pelo cliente
+        /** Recebe e valida os dados de login enviados pelo cliente  */
         const login: UserLogin = await request.json();
 
-        // 2. Cria uma nova sessão com o banco de dados
+        /** Cria uma nova sessão com o banco de dados */
         const session = await db.getSession();
 
-        // 3. Instancia o serviço de usuário, passando a sessão
+        /** Instancia o serviço de usuário, passando a sessão */
         const service = new UserService(session);
 
-        // 4. Tenta autenticar o usuário com as credenciais fornecidas
+        /**  Tenta autenticar o usuário com as credenciais fornecidas */
         const response = await service.login(login);
 
-        // 5. Se não encontrar usuário ou a senha for inválida, retorna o erro 401
+        // Se não encontrar usuário ou a senha for inválida, retorna o erro 401
         if (!response) {
             return new Response(JSON.stringify({ error: "Invalid credentials" }), {
                 status: 401,
@@ -37,10 +37,11 @@ export async function POST(request: Request): Promise<Response> {
             });
         }
 
-        // 6. Retorna os dados do usuário autenticado
+        // Retorna os dados do usuário autenticado
         return new Response(JSON.stringify({
             user: response,
-            token: SecurityHandler.generateTokenFromId(String(response.id)), // Gera um token JWT com validade de 1 hora
+            // Gera um token JWT com validade de 1 hora
+            token: SecurityHandler.generateTokenFromId(String(response.id)),
         }), {
             status: 200,
             headers: {
--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,39 +1,37 @@
 import db from "@/core/db";
 import UserService from "@/services/user";
 import { UserRequest } from "@/schemas/user";
-// import { json } from "stream/consumers";
 
 
-/*
+/**
     Função responsável por registrar(ativar a conta) um novo usuário
 
-    1. Recebe os dados de cadastro via request.
-    2. Cria uma sessão no banco de dados.
-    3. Instancia o serviço de usuário.
-    4. Verifica se já existe um usuário com o mesmo e-mail.
-    5. Se não existir, cadastra o novo usuário.
-    6. Retorna erro 409 se o e-mail já estiver cadastrado.
-    7. Retorna os dados do usuário cadastrado em caso de sucesso.
+    - Recebe os dados de cadastro via request.
+    - Cria uma sessão no banco de dados.
+    - Instancia o serviço de usuário.
+    - Verifica se já existe um usuário com o mesmo e-mail.
+    - Se não existir, cadastra o novo usuário.
+    - Retorna erro 409 se o e-mail já estiver cadastrado.
+    - Retorna os dados do usuário cadastrado em caso de sucesso.
 */
 
 export async function POST (request: Request): Promise<Response> {
     try {
-        // 1. Recebe e valida os dados de cadastro enviados pelo cliente
+        /** Recebe e valida os dados de cadastro enviados pelo cliente */
         const userData: UserRequest = await request.json();
 
-        // 2. Cria uma nova sessão(aqui é necessário mudar) com o banco de dados
+        /** Cria uma nova sessão com o banco de dados */
         const session = await db.getSession();
 
-        // 3. Instancia o serviço de usuário, passando a sessão
+        /** Instancia o serviço de usuário, passando a sessão */
         const service = new UserService(session);
 
 
         try {
-            // 4. Tenta adicionar o usuário(verifica se já existe internamente)
-
+            /** Tenta adicionar o usuário(verifica se já existe internamente) */
             const newUser = await service.add(userData);
 
-            // 5. Retorna os dados do usuário cadastrado
+            // Retorna os dados do usuário cadastrado
             return new Response(JSON.stringify(newUser), {
                 status: 201,
                 headers: {
@@ -41,7 +39,7 @@ export async function POST (request: Request): Promise<Response> {
                 },
             });
         } catch (error: any) {
-            // 6. Se o erro for devido a "usuário já existente", retorna 409
+            // Se o erro for devido a "usuário já existente", retorna 409
             if (error.message === "User already exists") {
                 return new Response(JSON.stringify({ error: "User already exists" }), {
                     status: 409,
@@ -58,7 +56,6 @@ export async function POST (request: Request): Promise<Response> {
             },
         });
     }
-    // Fallback return in case no other return was hit
     return new Response(JSON.stringify({ error: "Unknown error" }), {
         status: 500,
         headers: {
