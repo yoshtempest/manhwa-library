1cb0a2d7c985dd6820659560c1781a450583f720
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-07-14 16:05:04 -0300

    new: fetchBooks function and useState to favorited + favorite

--- src/frontend/components/Book/BookList/index.tsx
+++ src/frontend/components/Book/BookList/index.tsx
@@ -1,26 +1,51 @@
+"use client";
+
 import styles from './styles.module.css';
 import BookImage from '@/frontend/components/Book/BookImage';
 import BookRating from '@/frontend/components/Book/BookNotes';
 import { getBooks } from '@/mocks/books';
+import React, { useState } from 'react';
 
 
-async function Books() {
-  const books = await getBooks();
+async function fetchBooks() {
+    return await getBooks();
+}
+
+function Books() {
+  const [books, setBooks] = useState<any[]>([]);
+
+    React.useEffect(() => {
+        fetchBooks().then(setBooks);
+    }, []);
+
+    const toggleFavorite = (id: string) => {
+        setBooks((prevBooks) =>
+            prevBooks.map((book) =>
+                book.id === id
+                    ? { ...book, is_favorite: !book.is_favorite }
+                    : book
+            )
+        );
+    };
   return (
     <div className={styles.Container}>
       {books.map((book) => (
         <div key={book.id}>
           <h2 className={styles.Title}>{book.title}</h2>
+
           <BookImage
             imagePath={book.image_path ?? ''} // fallback to empty string if undefined
             alt={`Capa do livro ${book.title}`}
           />
+
           <div className={styles.NotesContainer}>
             <BookRating averageRating={book.note}/>
             <p className={styles.Note}>{book.note}</p>
           </div>
-          <p className={styles.Favorite}>
-            {book.is_favorite ? '❤️' : '🤍'} {book.is_favorite ? 'Favoritado' : 'Favoritar'}
+
+          <p className={styles.Favorite}
+            onClick={() => toggleFavorite(book.id)}>
+            {book.is_favorite ? '❤️ Favoritado' : '🤍 Favoritar'}
           </p>
         </div>
       ))}
