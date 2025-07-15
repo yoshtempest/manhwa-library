e75db56093e9c5da9c7341695537cfc90dbf62ac
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-07-15 16:48:26 -0300

    new: commenting the code

--- src/schemas/book.ts
+++ src/schemas/book.ts
@@ -1,6 +1,17 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { GenreResponse } from "./genre";
 
+
+/**
+ * BookRequest Schema Attributes:
+ * - title: string
+ * - author: string
+ * - description: string
+ * - note: number
+ * - image_path: string | StaticImport
+ * - likes: Array<number>
+ * - genres: Array<string> // Array of genre IDs
+*/
 export interface BookRequest {
     title: string;
     author: string;
@@ -11,6 +22,17 @@ export interface BookRequest {
     genres: Array<string>; // Array de IDs de gêneros
 }
 
+/**
+ * BookResponse Schema Attributes:
+ * - id: string
+ * - title: string
+ * - author: string
+ * - genres: Array<GenreResponse>
+ * - is_favorite: boolean
+ * - description: string
+ * - note: number
+ * - image_path?: string
+*/
 export interface BookResponse {
     id: string;
     title: string;
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,34 +1,34 @@
-/**UserRequest Schema
- * Attributes:
+/**
+ * UserRequest Schema Attributes:
  * - username: string
  * - email: string
  * - password: string
- */
+*/
 export interface UserRequest {
     username: string;
     email: string;
     password: string;
 }
 
-/**UserLogin Schema
- * Attributes:
+/**
+ * UserLogin Schema Attributes:
  * - email: string
  * - password: string
- */
+*/
 export interface UserLogin {
     email: string;
     password: string;
 }
 
-/**UserResponse Schema
- * Attributes:
- * id: string
- * username: string
- * email: string
- * active: boolean
- * created_at: string
- * updated_at: string
- */
+/**
+ * UserResponse Schema Attributes:
+ * - id: string
+ * - username: string
+ * - email: string
+ * - active: boolean
+ * - created_at: string
+ * - updated_at: string
+*/
 export interface UserResponse {
     id: string;
     username: string;
@@ -38,19 +38,19 @@ export interface UserResponse {
     updated_at: string;
 }
 
-/**TokenData Schema
- * Attributes:
+/**
+ * TokenData Schema Attributes:
  * - id: string
- */
+*/
 export interface TokenData {
     id: string
 }
 
-/**TokenResponse Schema
- * Attributes:
+/**
+ * TokenResponse Schema Attributes:
  * - token: string
  * - user: UserResponse
- */
+*/
 export interface TokenResponse {
     token: string;
     user: UserResponse;
