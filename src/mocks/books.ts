d040007ba956bb7b0445f28757749c0f54ac0002
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-10-04 13:54:57 -0300

    fix: problem with height size of SideBar and Container of page.tsx + responsivity in BookList Container + height size of burger Content

--- src/mocks/books.ts
+++ src/mocks/books.ts
@@ -44,6 +44,46 @@ export async function getBooks(bookTitle?: string):  Promise<BookResponse[]> {
             image_path: IMAGES.sobrevivendoComoUmBarbaro.src,
             is_favorite: false
         },
+                {
+            id: "5",
+            title: "Imperador Mágico",
+            author: "Ye Xiao",
+            genres: ["Action", "Fantasy"],
+            description: "Zhuo Yifan era um imperador mágico ou poderia ser chamado de imperador demônio porque ele tinha um livro do imperador antigo chamado Livro dos Nove Segredos, ele foi alvo de todos os especialistas e foi traído e morto por seus alunos. Então sua alma entra e volta à vida em um servo da família chamado Zhuo Fan. Alguma magia demoníaca o impede, ele deve unir as memórias de criança e não pode ignorar a família e a amante a quem serve.",
+            note: 4.5,
+            image_path: IMAGES.imperadorMagico.src,
+            is_favorite: false
+        },
+        {
+            id: "6",
+            title: "O Melhor Amigo do Mundo",
+            author: "Chugong",
+            genres: ["Action", "Fantasy"],
+            description: "Em um mundo onde caçadores humanos que possuem habilidades mágicas devem lutar contra monstros mortais para proteger a raça humana de certa aniquilação, um caçador notoriamente fraco chamado Sung Jinwoo se encontra em uma luta aparentemente interminável pela sobrevivência. Um dia, depois de sobreviver por pouco a uma masmorra dupla esmagadoramente poderosa que quase acaba com todo o seu grupo, um programa misterioso chamado Sistema o escolhe como seu único jogador e, por sua vez, dá a ele a habilidade extremamente rara de subir de nível em força, possivelmente além de qualquer limites conhecidos. Jinwoo então parte em uma jornada enquanto luta contra todos os tipos de inimigos, tanto homens quanto monstros, para descobrir os segredos das masmorras e a verdadeira fonte de seus poderes.",
+            note: 5,
+            image_path: IMAGES.melhorAmigoDoMundo.src,
+            is_favorite: false
+        },
+        {
+            id: "7",
+            title: "O Restaurante Do Arquimago",
+            author: "Maslow",
+            genres: ["Action", "Fantasy"],
+            description: "Jinhyuk, um viciado completo, foi o único a ter visto o final da [Torre dos Testes], mas à medida que a popularidade do jogo diminui, fica difícil manter a vida com o jogo. Jinhyuk quer terminar o jogo assim porque ele viu o final. Naquele mesmo dia, a [Torre dos Testes] se tornou uma realidade. Jinhyuk, que conhece todos os elementos do jogo, controla tudo mais rápido do que qualquer um! “Vou te mostrar o que é um profissional de verdade.”",
+            note: 4.5,
+            image_path: IMAGES.restauranteDoArquimago.src,
+            is_favorite: false
+        },
+        {
+            id: "8",
+            title: "Sobrevivendo Como Um Bárbaro",
+            author: "Maslow",
+            genres: ["Action", "Fantasy"],
+            description: "Jinhyuk, um viciado completo, foi o único a ter visto o final da [Torre dos Testes], mas à medida que a popularidade do jogo diminui, fica difícil manter a vida com o jogo. Jinhyuk quer terminar o jogo assim porque ele viu o final. Naquele mesmo dia, a [Torre dos Testes] se tornou uma realidade. Jinhyuk, que conhece todos os elementos do jogo, controla tudo mais rápido do que qualquer um! “Vou te mostrar o que é um profissional de verdade.”",
+            note: 4,
+            image_path: IMAGES.sobrevivendoComoUmBarbaro.src,
+            is_favorite: false
+        },
     ]
     // filtro para buscar os livros por nome
     const book = bookTitle
